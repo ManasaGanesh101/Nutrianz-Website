64ab52ead7619a11aee18ec1ffef58e9d6cca160
ManasaGanesh101 <manasa.ganesh2022@vitstudent.ac.in>
2026-06-07 09:20:41 +0530

    UI zoom adjust update

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -4,7 +4,7 @@ function Intro(){
     return(
         <>
         <div className="relative">
-            <img src={image1} className="w-full h-[110vh] object-cover object-center"></img>
+            <img src={image1} className="w-full h-[120vh] object-cover object-center"></img>
             {/* black overlay for readability*/}<div className="absolute inset-0 bg-black/30" />
             <div className="absolute inset-0 flex items-center justify-center">
                 <Typewriter />
